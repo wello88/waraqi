fd4aacb0b426d8851e3ba50cb80d3e8b118c09ca
wello88 <abdow8896@gmail.com>
2025-08-11 01:44:26 +0300

    connection online db

--- db/connection.js
+++ db/connection.js
@@ -2,34 +2,34 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 import path from 'path';
 
-// // Load environment variables
-// dotenv.config({ path: path.resolve('./config/.env') })
-// const sequelize = new Sequelize(process.env.DB_NAME,
-//   process.env.DB_USER, String(process.env.DB_PASSWORD),
-//   {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: process.env.DB_DIALECT || 'postgres',
-//     logging: false, // Set to false to disable SQL logging
-//     dialectOptions: process.env.DB_SSL === "true" ? {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     } : {},
-//   });
+// Load environment variables
+dotenv.config({ path: path.resolve('./config/.env') })
+const sequelize = new Sequelize(process.env.DB_NAME,
+  process.env.DB_USER, String(process.env.DB_PASSWORD),
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || 'postgres',
+    logging: false, // Set to false to disable SQL logging
+    dialectOptions: process.env.DB_SSL === "true" ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    } : {},
+  });
 
 
 
 
-// connect sequelize to the database using aiven
-const sequelize = new Sequelize('waraqi', 'postgres', '123', {
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
-  logging: false, // Disable SQL logging
+// // connect sequelize to the database using aiven
+// const sequelize = new Sequelize('waraqi', 'postgres', '123', {
+//   host: 'localhost',
+//   port: 5432,
+//   dialect: 'postgres',
+//   logging: false, // Disable SQL logging
 
-});
+// });
 
 // // connect sequelize to the database using aiven
 // const sequelize = new Sequelize('carmate', 'postgres', '1234', {
